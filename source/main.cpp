5b26daa670c069f1aebeec2bc47411da60bfab64
MattGuerrette <direct3Dtutorials@gmail.com>
2016-02-18 13:50:52 -0500

    updated main.

--- source/main.cpp
+++ source/main.cpp
@@ -17,49 +17,13 @@
 #include <ht_debug.h>
 #include <ht_file.h>
 #include <HatchitTest.h>
-#include <ht_model.h>
-#include <thread>
 
 using namespace Hatchit;
 using namespace Hatchit::Core;
 using namespace Hatchit::Game;
 
-typedef std::shared_ptr<Resource::Model> ModelPtr;
-
 int main(int argc, char* argv[])
 {
-    const char* names[4] =
-    {
-        "Raptor.obj",
-        "floor.obj",
-        "monkey.obj",
-        "test_dragon.dae"
-    };
-    std::vector<ModelPtr> _Models;
-    for (int i = 0; i < 4; i++)
-        _Models.push_back(std::make_shared<Resource::Model>());
-    std::thread t([](const char* fileNames[4], std::vector<ModelPtr>& models) {
-        for (int i = 0; i < 4; i++)
-        {
-            File file;
-            try
-            {
-                file.Open(os_exec_dir() + fileNames[i], FileMode::ReadText);
-
-                models[i]->VInitFromFile(&file);
-            }
-            catch (std::exception& e)
-            {
-#ifdef _DEBUG
-                DebugPrintF("%s\n", e.what());
-#endif
-            }
-
-        }
-
-    }, names, std::ref(_Models));
-    t.join();
-
     INIReader settings;
 
     File file;
