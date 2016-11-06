9547973b3b623335e6b73c570b6f5bae625bb757
MattGuerrette <direct3Dtutorials@gmail.com>
2016-11-05 23:44:19 -0500

    removed guid from json test

--- core_test/JsonTest.cpp
+++ core_test/JsonTest.cpp
@@ -23,8 +23,7 @@ static const JSON TestJson = R"(
         "message": "Hello, world!",
         "meaning_of_life": 42,
         "pi": 3.1415926535897932384626433832795,
-        "is_civil_war_out_yet": false,
-        "my_guid": "{cbe9644b-bd06-4391-9e9a-57913180254f}"
+        "is_civil_war_out_yet": false
     }
 )"_json;
 
@@ -33,24 +32,19 @@ static const std::string TestString = "Hello, world!";
 static const int64_t     TestInt    = 42;
 static const double      TestDouble = 3.1415926535897932384626433832795;
 static const bool        TestBool   = false;
-static       Guid        TestGuid;
 
 TEST(JsonTest, Extraction)
 {
-    ASSERT_TRUE(Guid::Parse("{cbe9644b-bd06-4391-9e9a-57913180254f}", TestGuid));
-
     std::string jsonString;
     int64_t jsonInt;
     double jsonDouble;
     bool jsonBool;
-    Guid jsonGuid;
 
 #if defined(_DEBUG) || defined(DEBUG)
     ASSERT_TRUE(JsonExtract(TestJson, "message", jsonString));
     ASSERT_TRUE(JsonExtract(TestJson, "meaning_of_life", jsonInt));
     ASSERT_TRUE(JsonExtract(TestJson, "pi", jsonDouble));
     ASSERT_TRUE(JsonExtract(TestJson, "is_civil_war_out_yet", jsonBool));
-    ASSERT_TRUE(JsonExtract(TestJson, "my_guid", jsonGuid));
 #else
     try
     {
@@ -70,5 +64,4 @@ TEST(JsonTest, Extraction)
     EXPECT_EQ(TestInt, jsonInt);
     EXPECT_EQ(TestDouble, jsonDouble);
     EXPECT_EQ(TestBool, jsonBool);
-    EXPECT_EQ(TestGuid, jsonGuid);
 }
