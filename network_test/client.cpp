c74328d290aef9f3547dc1f6a22e098f3f63cc5e
MattGuerrette <direct3Dtutorials@gmail.com>
2016-11-13 00:44:59 -0500

    added command line parsing for address and port

--- network_test/client.cpp
+++ network_test/client.cpp
@@ -11,16 +11,31 @@ int main(int argc, char* argv[])
 {
     Network::Initialize();
 
-    SocketAddress addr("127.0.0.1", 1111);
-    TCPSocketPtr connection = std::make_shared<TCPSocket>();
-    if(connection->Connect(addr) == 0)
+    /**
+     * Check command arguments.
+     * [param] Host address
+     * [param] Port number
+     */
+    if(argc > 2)
     {
-        HT_DEBUG_PRINTF("Connected!\n");
+        SocketAddress address(argv[1], std::atoi(argv[2]));
+        TCPSocketPtr connection = std::make_shared<TCPSocket>();
+        if(connection->Connect(address) == 0)
+        {
+            HT_DEBUG_PRINTF("Connected!\n");
+        }
+        else
+        {
+            HT_DEBUG_PRINTF("Failed to connect to host server.\n");
+        }
     }
     else
     {
-        HT_DEBUG_PRINTF("Failed to connect to host server.\n");
+        HT_DEBUG_PRINTF("Invalid arguments.\n");
+        HT_DEBUG_PRINTF("Usage: Client [address] [port]\n");
     }
+
+
 
     Network::Shutdown();
     return 0;
--- network_test/server.cpp
+++ network_test/server.cpp
@@ -11,20 +11,30 @@ int main(int argc, char* argv[])
 {
     Network::Initialize();
 
-    SocketAddress addr("129.21.90.242", 4040);
-    TCPSocketPtr listen = std::make_shared<TCPSocket>();
-    listen->Bind(addr);
-    listen->Listen();
-
-    TCPSocketPtr connection = listen->Accept();
-    if(!connection)
+    /**
+     * Check command arguments.
+     * [param] Host address
+     * [param] Port number
+     */
+    if(argc > 2)
     {
-        HT_DEBUG_PRINTF("Failed to accept client connection.\n");
+        SocketAddress address(argv[1], std::atoi(argv[2]));
+        TCPSocketPtr listen = std::make_shared<TCPSocket>();
+        listen->Bind(address);
+        listen->Listen();
+
+        TCPSocketPtr connection = listen->Accept();
+        if (!connection) {
+            HT_DEBUG_PRINTF("Failed to accept client connection.\n");
+        } else {
+            HT_DEBUG_PRINTF("Client[%s] connected.\n", connection->GetAddress().ToString());
+            HT_DEBUG_PRINTF("Listen[%s]\n", address.ToString());
+        }
     }
     else
     {
-        HT_DEBUG_PRINTF("Client[%s] connected.\n", connection->GetAddress().ToString());
-        HT_DEBUG_PRINTF("Listen[%s]\n", addr.ToString());
+        HT_DEBUG_PRINTF("Invalid arguments.\n");
+        HT_DEBUG_PRINTF("Usage: Server [address] [port]\n");
     }
 
     Network::Shutdown();
