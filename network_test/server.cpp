873017c336d8bbb1ed0e373d2efe6edb46c78bac
MattGuerrette <direct3Dtutorials@gmail.com>
2016-11-13 00:16:11 -0500

    add test print of listen socket address

--- network_test/server.cpp
+++ network_test/server.cpp
@@ -11,12 +11,12 @@ int main(int argc, char* argv[])
 {
     Network::Initialize();
 
-    SocketAddress addr("127.0.0.1", 1111);
+    SocketAddress addr("129.21.90.242", 4040);
     TCPSocketPtr listen = std::make_shared<TCPSocket>();
     listen->Bind(addr);
     listen->Listen();
 
-    TCPSocketPtr connection = listen->Accept(addr);
+    TCPSocketPtr connection = listen->Accept();
     if(!connection)
     {
         HT_DEBUG_PRINTF("Failed to accept client connection.\n");
@@ -24,6 +24,7 @@ int main(int argc, char* argv[])
     else
     {
         HT_DEBUG_PRINTF("Client[%s] connected.\n", connection->GetAddress().ToString());
+        HT_DEBUG_PRINTF("Listen[%s]\n", addr.ToString());
     }
 
     Network::Shutdown();
